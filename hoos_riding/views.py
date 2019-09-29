84914c737806af4fc7299941dc594fe35a0eb288
Wynne Barsanti <wynnebarsanti@Wynnes-MacBook-Pro.local>
2019-09-29 15:03:16 -0400

    created basic views and routing

--- hoos_riding/views.py
+++ hoos_riding/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def login(request):
+    return HttpResponse('<h1>Log In Page</h1>')
+
+
+def find(request):
+    return HttpResponse('<h1>Find a Ride</h1>')
+
+
+def post(request):
+    return HttpResponse('<h1>Post a Ride</h1>')
+
+
+def profile(request):
+    return HttpResponse('<h1>User Profile</h1>')
+
