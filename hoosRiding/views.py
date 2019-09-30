157c152dcfc16bb1d79b30ec6c2ecab485369c0e
Wynne Barsanti <wynnebarsanti@Wynnes-MacBook-Pro.local>
2019-09-30 15:03:15 -0400

    routing via templates

--- hoosRiding/views.py
+++ hoosRiding/views.py
@@ -6,17 +6,17 @@ from django.template import loader
 
 
 def login(request):
-    return HttpResponse('<h1>Login Page</h1>')
+    return render(request, 'hoosRiding/login.html')
 
 
 def find(request):
-    return HttpResponse('<h1>Find a Ride</h1>')
+    return render(request, 'hoosRiding/find_ride.html')
 
 
 def post(request):
-    return HttpResponse('<h1>Post a Ride</h1>')
+    return render(request, 'hoosRiding/post_ride.html')
 
 
 def profile(request):
-    return HttpResponse('<h1>User Profile</h1>')
+    return render(request, 'hoosRiding/profile.html')
 
--- jawgz/settings.py
+++ jawgz/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hoosRiding.apps.HoosRidingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
