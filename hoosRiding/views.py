b5690db5fd4900d6454e428181de430aa54d3f4c
Wynne Barsanti <wynnebarsanti@Wynnes-MacBook-Pro.local>
2019-09-30 15:24:43 -0400

    added css for header and base.html

--- hoosRiding/views.py
+++ hoosRiding/views.py
@@ -6,17 +6,17 @@ from django.template import loader
 
 
 def login(request):
-    return render(request, 'hoosRiding/login.html')
+    return render(request, 'hoosRiding/login.html', {'title': 'Login'})
 
 
 def find(request):
-    return render(request, 'hoosRiding/find_ride.html')
+    return render(request, 'hoosRiding/find_ride.html', {'title': 'Find'})
 
 
 def post(request):
-    return render(request, 'hoosRiding/post_ride.html')
+    return render(request, 'hoosRiding/post_ride.html', {'title': 'Post'})
 
 
 def profile(request):
-    return render(request, 'hoosRiding/profile.html')
+    return render(request, 'hoosRiding/profile.html', {'title': 'Profile'})
 
