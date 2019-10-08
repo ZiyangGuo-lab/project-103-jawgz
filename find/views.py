84a0e4690db6a69b87fcd54abaebfec6688834ee
garrett-levy <gjl8en@virginia.edu>
2019-10-08 19:28:58 -0400

    adding postings to find-ride page

--- find/views.py
+++ find/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Posting
 from django.views import generic
+from datetime import datetime
 
 # def find(request):
 #     return render(request, 'find/find_ride.html', {'title': 'Find'})
@@ -11,3 +12,10 @@ class findView(generic.ListView):
 
     def get_queryset(self):
         return Posting.objects.all()
+
+
+# def order_by_date(request):
+#     posting = Posting()
+#     return Posting.objects.filter(
+#         pub_date__lte=datetime.now()
+#     ).order_by('-date')
--- jawgz/urls.py
+++ jawgz/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('login/', include('login.urls')),
     path('post/', include('post.urls')),
