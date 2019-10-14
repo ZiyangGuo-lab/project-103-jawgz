307de7732f5e292fa5ee06e201137bb6de4edc53
John Saunders <johnsaunders@d-172-25-98-198.dhcp.virginia.edu>
2019-10-14 15:36:00 -0400

    added test cases

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from login.models import User
 
-# Create your tests here.
+class LoginTestCase(TestCase):
+
+    newUser = User()
+
+    def setUp(self):
+        # create user
+        self.newUser.name = "john doe"
+        self.newUser.username = "username123"
+        self.newUser.password = "password456"
+        self.newUser.uid = hash(self.newUser.name + self.newUser.username + self.newUser.name[::-1])
+        self.newUser.save()  # save user
+
+    def test_can_create_users(self):
+
+        #query this user
+        query = User.objects.filter(username=self.newUser.username, password=self.newUser.password)
+
+        #confirm this is the user
+        self.assertEqual(query.count(), 1)
+        userQueried = query[0]
+        self.assertEqual(userQueried.name, "john doe")
+        self.assertEqual(userQueried.username, "username123")
+        self.assertEqual(userQueried.password, "password456")
+        self.assertEqual(userQueried.uid, str(hash(userQueried.name + userQueried.username + userQueried.name[::-1])))
+
+    def tearDown(self):
+        query = User.objects.filter(username=self.newUser.username, password=self.newUser.password)
+        query.delete()
