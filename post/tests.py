de6c1f6f2f7935bfe88f7c97d4662902b3e01281
Yuxin <yw7vv@virginia.edu>
2019-10-20 18:51:21 -0400

    test case changed

--- post/tests.py
+++ post/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .forms import *
 from find.models import *
+from django.contrib.auth.models import User
 
 
 # Create your tests here.
@@ -9,19 +10,17 @@ class postRideTest(TestCase):
 	def test_form_validation(self):
 		form1 = postRide(data = {
 			"location_to" : " Charlottesville, VA", 
-			"location_from": "FairFax, VA",
-			"driver_name": "Bob", 
+			"location_from": "FairFax, VA", 
 			"vehicle_model": "Honda",
 			"price" : 20, 
-			"riding_date": "2019-10-18 14:00", 
+			"num_passengers": 2, 
 			})
 		form2 = postRide(data = {
 			"location_to" : " Charlottesville, VA", 
 			"location_from": "FairFax, VA",
-			"driver_name": "Bob", 
-			"vehicle_model": "Honda",
+			"vehicle_model": "",
 			"price" : 20, 
-			"riding_date": "2019", 
+			"num_passengers": 2,  
 			})
 
 		self.assertFalse(form2.is_valid())
@@ -31,36 +30,46 @@ class postRideTest(TestCase):
 		form1 = postRide(data = {
 			"location_to" : " Charlottesville, VA", 
 			"location_from": "FairFax, VA",
-			"driver_name": "Bob", 
 			"vehicle_model": "",
 			"price" : 20, 
-			"riding_date": "2019-10-18 14:00", 
+			"num_passengers": 2, 
 			})
+
 		form2 = postRide(data = {
 			"location_to" : "", 
-			"location_from": "FairFax, VA",
-			"driver_name": "Bob", 
+			"location_from": "FairFax, VA", 
 			"vehicle_model": "Honda",
 			"price" : 20, 
-			"riding_date": "2019", 
+			"num_passengers": 2, 
 			})
+		form3 = postRide(data = {
+			"location_to" : "", 
+			"location_from": "FairFax, VA", 
+			"vehicle_model": "Honda",
+			"price" : " ", 
+			"num_passengers": 2, 
+			})			
+
 
 		self.assertFalse(form2.is_valid())
 		self.assertFalse(form1.is_valid())
+		self.assertFalse(form3.is_valid())
 
 	def test_form_content(self):
 		form1 = postRide(data = {
 			"location_to" : "Charlottesville, VA", 
 			"location_from": "FairFax, VA",
-			"driver_name": "Bob", 
 			"vehicle_model": "Honda",
 			"price" : 20, 
-			"riding_date": "2019-10-18 14:00", 
+			"num_passengers": 2,  
 			})
 		form1 = form1.save()
 
 		self.assertEqual(form1.location_to, "Charlottesville, VA")
-		self.assertEqual(form1.driver_name, "Bob")
+		self.assertEqual(form1.num_passengers, 2)
+		self.assertEqual(form1.vehicle_model, "Honda")
+
+	
 
 
 
