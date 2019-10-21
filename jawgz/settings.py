6b0a387692a363f969ffae740be1ec7752cdcd56
Yuxin <yw7vv@virginia.edu>
2019-10-21 17:30:57 -0400

    change

--- jawgz/settings.py
+++ jawgz/settings.py
@@ -148,19 +148,12 @@ STATIC_URL = '/static/'
 # except ImportError:
 #     found = False
 
-try:
+
+if 'HEROKU' in os.environ:
     import dj_database_url
     DATABASES['default'] =  dj_database_url.config()
     import django_heroku
     django_heroku.settings(locals())
-except ImportError:
-    found = False
-
-# if 'HEROKU' in os.environ:
-#     import dj_database_url
-#     DATABASES['default'] =  dj_database_url.config()
-#     import django_heroku
-#     django_heroku.settings(locals())
 
 # MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # MEDIA_URL = '/media/'
