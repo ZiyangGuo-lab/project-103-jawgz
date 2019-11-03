a37b8f9238830b114100f51ed294c89cf7c5ea00
Yuxin <yw7vv@virginia.edu>
2019-11-03 18:06:45 -0500

    sort works

--- find/urls.py
+++ find/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.findView.as_view(), name='find-ride')
+    path('', views.findView.as_view(), name='find-ride'),
+    # path('', views.findRide, name='find-ride'),
+    path('sortByPrice', views.priceView.as_view(), name='sortByPrice'),
+    path('sortByDate', views.dateView.as_view(), name='sortByDate')
 ]
 
--- find/views.py
+++ find/views.py
@@ -2,18 +2,39 @@ from django.shortcuts import render
 from .models import Posting
 from django.views import generic
 from datetime import datetime
-
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+import time
+from django.db.models import Q
 
 class findView(generic.ListView):
     template_name = 'find/find_ride.html'
     context_object_name = 'postings_list'
 
     def get_queryset(self):
-        return Posting.objects.all()
+    	# print("find ride")
+    	return Posting.objects.all()
 
 
-# def order_by_date(request):
-#     posting = Posting()
-#     return Posting.objects.filter(
-#         pub_date__lte=datetime.now()
-#     ).order_by('-date')
+class dateView(generic.ListView):
+    template_name = 'find/find_ride_date.html'
+    context_object_name = 'postings_list'
+
+    def get_queryset(self):
+    	# print("find ride")
+    	return Posting.objects.all().order_by('-riding_date')
+
+class priceView(generic.ListView):
+    template_name = 'find/find_ride_price.html'
+    context_object_name = 'postings_list'
+
+    def get_queryset(self):
+    	return Posting.objects.all().order_by('price')
+
+
+# class searchView(generic.ListView):
+#     template_name = 'find/find_ride_search.html'
+#     context_object_name = 'postings_list'
+
+#     def get_queryset(self):
+#     	return Posting.objects.all().order_by('price')
