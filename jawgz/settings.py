d131aca209c18074f85d4c4c8cf4ac6b1336ccdc
John Saunders <johnsaunders@d-172-25-203-177.dhcp.virginia.edu>
2019-11-03 11:35:19 -0500

    custom rider model

--- jawgz/settings.py
+++ jawgz/settings.py
@@ -34,7 +34,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '06HeNGhqjrk0X4VP0aq_ZYlP'
 
 LOGIN_URL = '/auth/login/google-oauth2/'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/login/createNewUser'
 LOGOUT_REDIRECT_URL = '/'
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
@@ -121,6 +121,9 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
+SOCIAL_AUTH_GOOGLE_OAUTH2_AUTH_EXTRA_ARGUMENTS = {
+    'hd': 'virginia.edu'
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+# from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class Rider(models.Model):
+    username = models.CharField(max_length=200)
+    cellphone = models.CharField(max_length=200)
+    car_type = models.CharField(max_length=200)
+    license_plate = models.CharField(max_length=200)
+    rides_driven = models.TextField()#need to use this when upgrade to postgres --> ArrayField(models.CharField(max_length=20, blank=True),size=20)
+    rides_passenger = models.TextField()#ArrayField(models.CharField(max_length=20, blank=True), size=20)
+
+    def __str__(self):
+        return self.username
