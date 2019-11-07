d90543163745de34bc1ea2959373181d4eaada75
Wynne Barsanti <wynnebarsanti@Wynnes-MacBook-Pro.local>
2019-11-06 22:33:23 -0500

    profile is editable

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='user-profile')
+    path('', views.profile, name='user-profile'),
+    path('updateProfile', views.update_profile_form, name="update-profile")
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -8,26 +8,21 @@ from django.contrib.auth.models import User
 # Create your views here.
 def profile(request):
     id = request.user
-    print(id)
-    current_user = Rider.objects.filter(username=id)
+    user_matches = Rider.objects.filter(username=id)
+    current_user = user_matches[0]
+    # check that each field is not blank before updating
+    if (request.GET.get('license_plate') is not None):
+        current_user.license_plate = request.GET.get('license_plate')
+        current_user.save()
+    if (request.GET.get('cellphone') is not None):
+        current_user.cellphone = request.GET.get('cellphone')
+        current_user.save()
+    if (request.GET.get('car_type') is not None):
+        current_user.car_type = request.GET.get('car_type')
+        current_user.save()
+
     all_rides = Posting.objects.filter(driver_id=id)
     open_rides = Posting.objects.filter(driver_id=id)
     past_rides = Posting.objects.filter(driver_id=id)
     return render(request, 'user_profile/profile.html', {'title': 'Profile', 'id': id, 'current_user': current_user,
                                                          'open_rides': open_rides, 'past_rides': past_rides})
-
-
-def updateProfile(request):
-    print(request)
-    form = update_profile_form(request.POST)
-    if form.is_valid():
-        print('valid form')
-    current_user = Rider.objects.filter(username=id)
-
-    current_user.cellphone = request.POST.get('cellphone')
-    current_user.car_type = request.POST.get('car_type')
-    current_user.license_plate = request.POST.get('license_plate')
-    current_user.save()
-
-    print("current: ", current_user)
-    return render(request, 'user_profile/profile.html', {'title': 'Profile', 'id': id, 'current_user': current_user})
