39d5f91de98875554c774f44bc5fb40094dcacb4
John Saunders <johnsaunders@d-172-25-254-221.dhcp.virginia.edu>
2019-11-08 16:52:23 -0500

    design changes

--- find/tests.py
+++ find/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+# from .forms import *
+from find.models import *
+from django.contrib.auth.models import User
 
-# Create your tests here.
+# class searchViewTest(TestCase):
+
+# 	def setUp(self):
+# 		Posting.objects.create(location_to="Charlottesville, VA", location_from="FairFax, VA",vehicle_model="Honda", price=20,num_passengers=2)
+# # Create your tests here.
+
+	# class postingModelTest(TestCase):
+
+	# 	def create_Posting(self, location_to="Charlottesville, VA", location_from="FairFax, VA",vehicle_model="Honda", price=20,num_passengers=2,)
