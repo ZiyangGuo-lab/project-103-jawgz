4b042bf431c334e0c99f7a8430bb9597140d1d82
garrett-levy <gjl8en@virginia.edu>
2019-11-10 18:50:18 -0500

    testing postgres connection

--- jawgz/settings.py
+++ jawgz/settings.py
@@ -12,15 +12,15 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 
 import os, subprocess, dj_database_url
-import dotenv
+# import dotenv
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
+# dotenv_file = os.path.join(BASE_DIR, ".env")
+# if os.path.isfile(dotenv_file):
+#     dotenv.load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
