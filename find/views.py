75463dc534db871032bff487dd2fe084c1256275
John Saunders <johnsaunders@Johns-MacBook-Pro-3.local>
2019-11-11 10:24:43 -0500

    fixed test cases

--- find/views.py
+++ find/views.py
@@ -88,60 +88,60 @@ def formatDate(date):
 	return date[2] + '-' + str(months.index(date[0]) + 1) + '-' + (date[1])[:-1]
 
 
-# class findView(generic.ListView):
-# 	template_name = 'find/find_ride.html'
-# 	context_object_name = 'postings_list'
-# 	def get_queryset(self):
-# 		return Posting.objects.all()
+class findView(generic.ListView):
+	template_name = 'find/find_ride.html'
+	context_object_name = 'postings_list'
+	def get_queryset(self):
+		return Posting.objects.all()
 
-# class dateView(generic.ListView):
-#     template_name = 'find/find_ride_date.html'
-#     context_object_name = 'postings_list'
-#
-#     def get_queryset(self):
-#     	# print("find ride")
-#     	return Posting.objects.all().order_by('-riding_date')
-#
-# class priceView(generic.ListView):
-#     template_name = 'find/find_ride_price.html'
-#     context_object_name = 'postings_list'
-#
-#     def get_queryset(self):
-#     	return Posting.objects.all().order_by('price')
-#
-#
-# class searchView(generic.ListView):
-# 	model = Posting
-# 	template_name = 'find/find_ride_search.html'
-# 	context_object_name = 'postings_list'
-# 	# queryset = Posting.objects.filter(location_to='Virginia Beach, VA')
-#
-# 	def get_queryset(self):
-# 		location_to = self.request.GET.get('location_to')
-# 		print(self.request.GET.get('location_to')+"location_to")
-# 		location_from = self.request.GET.get('location_from')
-# 		year = self.request.GET.get('date_year')
-# 		month = self.request.GET.get('date_month')
-# 		day = self.request.GET.get('date_day')
-#
-# 		s = year+ '-' +month+ '-' +day
-# 		if location_to=="" and location_from != "" and s!="--":
-# 			return Posting.objects.filter(location_from=location_from, riding_date__date=s)
-# 		elif location_to!="" and location_from == "" and s!="--":
-# 			return Posting.objects.filter(location_to=location_to, riding_date__date=s)
-# 		elif location_to!="" and location_from != "" and s=="--":
-# 			return Posting.objects.filter(location_to=location_to, location_from=location_from)
-# 		elif location_to=="" and location_from == "" and s!="--":
-# 			return Posting.objects.filter(riding_date__date=s)
-# 		elif location_to !="" and location_from == "" and s=="--":
-# 			print("method goes here")
-# 			return Posting.objects.filter(location_to=location_to)
-# 		elif location_to =="" and location_from != "" and s=="--":
-# 			return Posting.objects.filter(location_from=location_from)
-# 		elif location_to =="" and location_from == "" and s=="--":
-# 			return Posting.objects.filter(location_from=location_from)
-# 		else:
-# 			return Posting.objects.filter(location_to=location_to, location_from=location_from, riding_date__date=s)
+class dateView(generic.ListView):
+    template_name = 'find/find_ride_date.html'
+    context_object_name = 'postings_list'
+
+    def get_queryset(self):
+    	# print("find ride")
+    	return Posting.objects.all().order_by('-riding_date')
+
+class priceView(generic.ListView):
+    template_name = 'find/find_ride_price.html'
+    context_object_name = 'postings_list'
+
+    def get_queryset(self):
+    	return Posting.objects.all().order_by('price')
+
+
+class searchView(generic.ListView):
+	model = Posting
+	template_name = 'find/find_ride_search.html'
+	context_object_name = 'postings_list'
+	# queryset = Posting.objects.filter(location_to='Virginia Beach, VA')
+
+	def get_queryset(self):
+		location_to = self.request.GET.get('location_to')
+		print(self.request.GET.get('location_to')+"location_to")
+		location_from = self.request.GET.get('location_from')
+		year = self.request.GET.get('date_year')
+		month = self.request.GET.get('date_month')
+		day = self.request.GET.get('date_day')
+
+		s = year+ '-' +month+ '-' +day
+		if location_to=="" and location_from != "" and s!="--":
+			return Posting.objects.filter(location_from=location_from, riding_date__date=s)
+		elif location_to!="" and location_from == "" and s!="--":
+			return Posting.objects.filter(location_to=location_to, riding_date__date=s)
+		elif location_to!="" and location_from != "" and s=="--":
+			return Posting.objects.filter(location_to=location_to, location_from=location_from)
+		elif location_to=="" and location_from == "" and s!="--":
+			return Posting.objects.filter(riding_date__date=s)
+		elif location_to !="" and location_from == "" and s=="--":
+			print("method goes here")
+			return Posting.objects.filter(location_to=location_to)
+		elif location_to =="" and location_from != "" and s=="--":
+			return Posting.objects.filter(location_from=location_from)
+		elif location_to =="" and location_from == "" and s=="--":
+			return Posting.objects.filter(location_from=location_from)
+		else:
+			return Posting.objects.filter(location_to=location_to, location_from=location_from, riding_date__date=s)
 
 
 
--- post/tests.py
+++ post/tests.py
@@ -10,56 +10,50 @@ class postRideTest(TestCase):
 	def test_form_validation(self):
 		form1 = postRide(data = {
 			"location_to" : " Charlottesville, VA", 
-			"location_from": "FairFax, VA", 
-			"vehicle_model": "Honda",
+			"location_from": "FairFax, VA",
 			"price" : 20, 
 			"num_passengers": 2, 
 			})
 		form2 = postRide(data = {
 			"location_to" : " Charlottesville, VA", 
 			"location_from": "FairFax, VA",
-			"vehicle_model": "",
 			"price" : 20, 
 			"num_passengers": 2,  
 			})
 
-		self.assertFalse(form2.is_valid())
+		self.assertTrue(form2.is_valid())
 		self.assertTrue(form1.is_valid())
 
 	def test_form_empty(self):
 		form1 = postRide(data = {
 			"location_to" : " Charlottesville, VA", 
 			"location_from": "FairFax, VA",
-			"vehicle_model": "",
 			"price" : 20, 
 			"num_passengers": 2, 
 			})
 
 		form2 = postRide(data = {
 			"location_to" : "", 
-			"location_from": "FairFax, VA", 
-			"vehicle_model": "Honda",
+			"location_from": "FairFax, VA",
 			"price" : 20, 
 			"num_passengers": 2, 
 			})
 		form3 = postRide(data = {
 			"location_to" : "", 
-			"location_from": "FairFax, VA", 
-			"vehicle_model": "Honda",
+			"location_from": "FairFax, VA",
 			"price" : " ", 
 			"num_passengers": 2, 
 			})			
 
 
 		self.assertFalse(form2.is_valid())
-		self.assertFalse(form1.is_valid())
+		self.assertTrue(form1.is_valid())
 		self.assertFalse(form3.is_valid())
 
 	def test_form_content(self):
 		form1 = postRide(data = {
 			"location_to" : "Charlottesville, VA", 
 			"location_from": "FairFax, VA",
-			"vehicle_model": "Honda",
 			"price" : 20, 
 			"num_passengers": 2,  
 			})
@@ -67,7 +61,6 @@ class postRideTest(TestCase):
 
 		self.assertEqual(form1.location_to, "Charlottesville, VA")
 		self.assertEqual(form1.num_passengers, 2)
-		self.assertEqual(form1.vehicle_model, "Honda")
 
 	
 
